86052cfa55c8162ad9c9417e6ebd2b9974e8b0a8
Graham <graham.gibbons@gmail.com>
2017-09-12 10:47:33 -0700

    Add example

--- docs/index.md
+++ docs/index.md
@@ -49,6 +49,45 @@ viewer(document.getElementById("canvas"),
 		]));
 ```
 
+### Skulls
+
+<example id="skulls"></example>
+
+```ts
+scene({
+	camera: orbit({
+		radius: value(3),
+		offset: value(-0.3, -0.4),
+		aperture: value(0.1)
+	}),
+	models: [
+		model({
+			shape: scale(value(10000), sphere()),
+			material: spotlight({
+				direction: value(1, 1, 0),
+				spread: value(0.1),
+				color: value(0.5),
+				ambient: value(1)
+			})
+		}),
+		model({
+			shape: plane(value(0, 1, 0), value(0.3)),
+			material: material({
+				color: value(0.5)
+			})
+		}),
+		model({
+			shape: repeat(value(1.3, 0, 1),
+				rotateZ(value(-Math.PI / 4), skull())),
+			material: material({
+				smoothness: value(0.99),
+				color: value(0.4, 0.6, 0.8)
+			})
+		})
+	]
+})
+```
+
 ### Truchet
 
 <example id="truchet"></example>
--- index.ts
+++ index.ts
@@ -26,6 +26,7 @@ import {
 	sierpinski,
 	skull,
 	smoothBox,
+	smoothUnion,
 	sphere,
 	spotlight,
 	stretch,
@@ -222,6 +223,41 @@ example("simple", scene({
 		})
 	]
 }), options({}));
+
+example("skulls", scene({
+	camera: orbit({
+		radius: value(3),
+		offset: value(-0.3, -0.4),
+		aperture: value(0.1)
+	}),
+	models: [
+		model({
+			shape: scale(value(10000), sphere()),
+			material: spotlight({
+				direction: value(1, 1, 0),
+				spread: value(0.1),
+				color: value(0.5),
+				ambient: value(1)
+			})
+		}),
+		model({
+			shape: plane(value(0, 1, 0), value(0.3)),
+			material: material({
+				color: value(0.5)
+			})
+		}),
+		model({
+			shape: repeat(value(1.3, 0, 1),
+				rotateZ(value(-Math.PI / 4), skull())),
+			material: material({
+				smoothness: value(0.99),
+				color: value(0.4, 0.6, 0.8)
+			})
+		})
+	]
+}), options({
+	stepFactor: 0.6
+}));
 
 example("truchet", scene({
 	camera: orbit({
